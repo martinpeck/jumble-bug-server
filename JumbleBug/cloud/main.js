b6cf578b99f009ab6d397d5600ae183fead2dc39
martinpeck <mpeck@hotmail.co.uk>
2014-04-07 23:01:32 +0100

    added some additional methods

--- JumbleBug/cloud/main.js
+++ JumbleBug/cloud/main.js
@@ -4,3 +4,20 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("test1", function(request, response) {
+  response.success("test1-response");
+});
+
+Parse.Cloud.define("findBug", function(request, response) {
+  var query = new Parse.Query("Bug");
+  query.equalTo("bugid", request.params.bugId);
+  query.find({
+    success: function(result) {
+      response.success(result[0]);
+    },
+    error: function() {
+      response.error("bug lookup failed");
+    }
+  });
+});
